640b09d45ca2a5b84e75d882abe4893c1b22255b
Colin Reilly <colinrei@gmail.com>
2017-09-08 14:09:49 -0500

    2017 portfolio upload

---
+++ js/custom.js
@@ -0,0 +1,172 @@
+$(".link-colors em.color-one").hover(function() {
+    $('.web-development-container .project-list').addClass("color-one");
+}, function() {
+    $('.web-development-container .project-list').removeClass("color-two color-three color-four color-five color-six color-seven color-eight color-nine color-ten color-eleven color-twelve");
+});
+
+$(".link-colors em.color-two").hover(function() {
+    $('.web-development-container .project-list').addClass("color-two");
+}, function() {
+    $('.web-development-container .project-list').removeClass("color-one color-three color-four color-five color-six color-seven color-eight color-nine color-ten color-eleven color-twelve");
+});
+
+$(".link-colors em.color-three").hover(function() {
+    $('.web-development-container .project-list').addClass("color-three");
+}, function() {
+    $('.web-development-container .project-list').removeClass("color-two color-one color-four color-five color-six color-seven color-eight color-nine color-ten color-eleven color-twelve");
+});
+
+$(".link-colors em.color-four").hover(function() {
+    $('.web-development-container .project-list').addClass("color-four");
+}, function() {
+    $('.web-development-container .project-list').removeClass("color-two color-three color-one color-five color-six color-seven color-eight color-nine color-ten color-eleven color-twelve");
+});
+
+$(".link-colors em.color-five").hover(function() {
+    $('.web-development-container .project-list').addClass("color-five");
+}, function() {
+    $('.web-development-container .project-list').removeClass("color-two color-three color-four color-one color-six color-seven color-eight color-nine color-ten color-eleven color-twelve");
+});
+
+$(".link-colors em.color-six").hover(function() {
+    $('.web-development-container .project-list').addClass("color-six");
+}, function() {
+    $('.web-development-container .project-list').removeClass("color-two color-three color-four color-five color-one color-seven color-eight color-nine color-ten color-eleven color-twelve");
+});
+
+
+
+
+
+$(".link-colors em.color-seven").hover(function() {
+    $('.design-container .project-list').addClass("color-seven");
+}, function() {
+    $('.design-container .project-list').removeClass("color-two color-three color-four color-five color-one color-six color-eight color-nine color-ten color-eleven color-twelve");
+});
+
+$(".link-colors em.color-eight").hover(function() {
+    $('.design-container .project-list').addClass("color-eight");
+}, function() {
+    $('.design-container .project-list').removeClass("color-two color-three color-four color-five color-one color-seven color-six color-nine color-ten color-eleven color-twelve");
+});
+
+$(".link-colors em.color-nine").hover(function() {
+    $('.design-container .project-list').addClass("color-nine");
+}, function() {
+    $('.design-container .project-list').removeClass("color-two color-three color-four color-five color-one color-seven color-eight color-six color-ten color-eleven color-twelve");
+});
+
+$(".link-colors em.color-ten").hover(function() {
+    $('.design-container .project-list').addClass("color-ten");
+}, function() {
+    $('.design-container .project-list').removeClass("color-two color-three color-four color-five color-one color-seven color-eight color-nine color-six color-eleven color-twelve");
+});
+
+$(".link-colors em.color-eleven").hover(function() {
+    $('.design-container .project-list').addClass("color-eleven");
+}, function() {
+    $('.design-container .project-list').removeClass("color-two color-three color-four color-five color-one color-seven color-eight color-nine color-ten color-six color-twelve");
+});
+
+$(".link-colors em.color-twelve").hover(function() {
+    $('.design-container .project-list').addClass("color-twelve");
+}, function() {
+    $('.design-container .project-list').removeClass("color-two color-three color-four color-five color-one color-seven color-eight color-nine color-ten color-eleven color-six");
+});
+
+
+
+$(".durbinspizza-project").hover(function() {
+    $('.durbinspizza-preview').addClass("show-design-image");
+}, function() {
+    $('.durbinspizza-preview').removeClass("show-design-image");
+});
+
+$(".lacrossemilling-project").hover(function() {
+    $('.lacrossemilling-preview').addClass("show-design-image");
+}, function() {
+    $('.lacrossemilling-preview').removeClass("show-design-image");
+});
+
+$(".drjackpatel-project").hover(function() {
+    $('.drjackpatel-preview').addClass("show-design-image");
+}, function() {
+    $('.drjackpatel-preview').removeClass("show-design-image");
+});
+
+$(".pawshappylife-project").hover(function() {
+    $('.pawshappylife-preview').addClass("show-design-image");
+}, function() {
+    $('.pawshappylife-preview').removeClass("show-design-image");
+});
+
+$(".cowbellecreamery-project").hover(function() {
+    $('.cowbellecreamery-preview').addClass("show-design-image");
+}, function() {
+    $('.cowbellecreamery-preview').removeClass("show-design-image");
+});
+
+$(".foodclubbrand-project").hover(function() {
+    $('.foodclubbrand-preview').addClass("show-design-image");
+}, function() {
+    $('.foodclubbrand-preview').removeClass("show-design-image");
+});
+
+$(".sweetpsbakeshop-project").hover(function() {
+    $('.sweetpsbakeshop-preview').addClass("show-design-image");
+}, function() {
+    $('.sweetpsbakeshop-preview').removeClass("show-design-image");
+});
+
+$(".naturesweettomatoesdevelopment-project").hover(function() {
+    $('.naturesweettomatoesdevelopment-preview').addClass("show-design-image");
+}, function() {
+    $('.naturesweettomatoesdevelopment-preview').removeClass("show-design-image");
+});
+
+
+
+
+
+
+$(".myhubbnetwork-project").hover(function() {
+    $('.myhubbnetwork-preview').addClass("show-design-image");
+}, function() {
+    $('.myhubbnetwork-preview').removeClass("show-design-image");
+});
+
+$(".naturesweettomatoesdesign-project").hover(function() {
+    $('.naturesweettomatoesdesign-preview').addClass("show-design-image");
+}, function() {
+    $('.naturesweettomatoesdesign-preview').removeClass("show-design-image");
+});
+
+$(".bestfreeapps-project").hover(function() {
+    $('.bestfreeapps-preview').addClass("show-design-image");
+}, function() {
+    $('.bestfreeapps-preview').removeClass("show-design-image");
+});
+
+$(".metvnetwork-project").hover(function() {
+    $('.metvnetwork-preview').addClass("show-design-image");
+}, function() {
+    $('.metvnetwork-preview').removeClass("show-design-image");
+});
+
+$(".wciumobile-project").hover(function() {
+    $('.wciumobile-preview').addClass("show-design-image");
+}, function() {
+    $('.wciumobile-preview').removeClass("show-design-image");
+});
+
+$(".voskosyogurt-project").hover(function() {
+    $('.voskosyogurt-preview').addClass("show-design-image");
+}, function() {
+    $('.voskosyogurt-preview').removeClass("show-design-image");
+});
+
+$(".zoolocaldeals-project").hover(function() {
+    $('.zoolocaldeals-preview').addClass("show-design-image");
+}, function() {
+    $('.zoolocaldeals-preview').removeClass("show-design-image");
+});
